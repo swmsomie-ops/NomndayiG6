2626b2cfaad201df04d3a92e890b82b2b6b5e46c
swmsomie-ops <swmsomie@gmail.com>
2026-09-24 15:38:09 +0200

    Added Dashboard class

--- src/test/java/Tests/NdosiLogInTest.java
+++ src/test/java/Tests/NdosiLogInTest.java
@@ -1,5 +1,6 @@
 package Tests;
 
+import Pages.LogInPage;
 import Utils.Base;
 import org.testng.annotations.Test;
 
@@ -9,5 +10,9 @@ public class NdosiLogInTest extends Base {
     public void LoginTest() {
         HomePage.checkIfNdosiWebsiteIsLoaded();
         HomePage.clickHomeLoginButton();
+        LogInPage.enterUsername("swmsomie@gmail.com");
+        LogInPage.enterPassword("Siya@MsM87");
+        LogInPage.clickLogInButton();
+
     }
 }
--- src/test/java/Utils/Base.java
+++ src/test/java/Utils/Base.java
@@ -1,10 +1,12 @@
 package Utils;
 
 import Pages.HomePage;
+import Pages.LogInPage;
 import org.openqa.selenium.WebDriver;
 
 public class Base {
 
     static final WebDriver driver = BrowserFactory.startBrowser("Chrome", "//https://ndosisimplifiedautomation.vercel.app");
     public HomePage HomePage = new HomePage(driver);
+    public LogInPage logInPage = new LogInPage(driver);
 }
