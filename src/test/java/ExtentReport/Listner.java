197d2b5309d0155e3e4126dfbfb4a4b05d13131b
swmsomie-ops <swmsomie@gmail.com>
2026-09-25 23:51:12 +0200

    Added Listner class

--- src/test/java/ExtentReport/Listner.java
+++ src/test/java/ExtentReport/Listner.java
@@ -1,4 +1,39 @@
 package ExtentReport;
 
-public class Listner {
-}
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestContext;
+import org.testng.ITestNGListener;
+import org.testng.ITestResult;
+
+public class Listner implements ITestNGListener {
+
+    private static ExtentReports extent;
+    private static ExtentTest extentTest;
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        extentTest = extent.createTest(result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        extentTest.log(Status.FAIL, "Test Case"+ result.getMethod().getMethodName()+"Has Failed");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        extentTest.log(Status.PASS, "Test Case"+ result.getMethod().getMethodName()+"Has Passed");
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        extentTest.log(Status.SKIP, "Test Case"+ result.getMethod().getMethodName()+"Has been Skipped");
+    }
+
+    @Override
+    public void onFinish(ITestContext result ) {
+        extent.flush();
+    }
+ }
